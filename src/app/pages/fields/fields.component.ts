86f54801e6fac3085c5433a9a52940e334a45424
Guillermo Piñate <gpinate@gmail.com>
2022-05-26 16:37:50 +0200

    heroes and fields api call with css

--- src/app/pages/fields/fields.component.ts
+++ src/app/pages/fields/fields.component.ts
@@ -1,15 +1,20 @@
+import { ApiFieldService } from './../../core/services/ApiField/api-field.service';
+import { IField } from './../../core/services/models/hero.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fields',
   templateUrl: './fields.component.html',
-  styleUrls: ['./fields.component.scss']
+  styleUrls: ['./fields.component.scss'],
 })
 export class FieldsComponent implements OnInit {
+  public fields?: IField[];
 
-  constructor() { }
+  constructor(private apiFieldService: ApiFieldService) {}
 
   ngOnInit(): void {
+    this.apiFieldService.getFields().subscribe((fields) => {
+      this.fields = fields;
+    });
   }
-
 }
--- src/app/pages/fields/fields.module.ts
+++ src/app/pages/fields/fields.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { FieldsRoutingModule } from './fields-routing.module';
 import { FieldsComponent } from './fields.component';
+import { FieldComponent } from './field/field.component';
 
 
 @NgModule({
   declarations: [
-    FieldsComponent
+    FieldsComponent,
+    FieldComponent
   ],
   imports: [
     CommonModule,
